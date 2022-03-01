93c5beecad677301f81e0dd28d22c8aff53c4d84
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-03-01 23:10:40 +0700

    update: refactoring simple simple code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,19 @@
-import org.jetbrains.compose.compose
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
     kotlin("jvm") version "1.5.21"
-    id("org.jetbrains.compose") version "1.0.0-alpha3"
     id("maven-publish")
 }
-
-/** Artifact groupId. */
-group = "com.frogobox"
-
-/** Artifact version. Note that "SNAPSHOT" in the version is not supported by bintray. */
-version = "1.0.3"
-
-/** This is from settings.gradle.kts. */
-val myArtifactId: String = rootProject.name
-
-/** This is defined above as `group`. */
-val myArtifactGroup: String = project.group.toString()
-
-/** This is defined above as `version`. */
-val myArtifactVersion: String = project.version.toString()
-
-/** My GitHub username. */
-val myGithubUsername = "amirisback"
-val myGithubDescription = "Repository Sample For Native Kotlin Library"
-val myGithubHttpUrl = "https://github.com/${myGithubUsername}/${myArtifactId}"
-val myGithubIssueTrackerUrl = "https://github.com/${myGithubUsername}/${myArtifactId}/issues"
-val myLicense = "Apache-2.0"
-val myLicenseUrl = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-
-val myDeveloperName = "Muhammad Faisal Amir"
-
 
 repositories {
     google()
     mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation(compose.desktop.currentOs)
 }
 
 java {
@@ -56,83 +25,26 @@ tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
 }
 
-compose.desktop {
-    application {
-        mainClass = "MainKt"
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = myArtifactId
-            packageVersion = myArtifactVersion
-        }
-    }
-}
+val VERSION_MAJOR = 1
+val VERSION_MINOR = 0
+val VERSION_PATCH = 4
 
+group = "com.frogobox"
+version = "$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH"
 
-val sourcesJar by tasks.creating(Jar::class) {
-    archiveClassifier.set("sources")
-    from(sourceSets.getByName("main").allSource)
-    from("LICENCE.md") {
-        into("META-INF")
-    }
-}
-
-// More info on `publishing`:
-//   https://docs.gradle.org/current/userguide/publishing_maven.html#publishing_maven:resolved_dependencies
-// More info on authenticating with personal access token (myDeveloperId and myArtifactName must be lowercase):
-//   https://docs.github.com/en/packages/guides/configuring-gradle-for-use-with-github-packages#authenticating-to-github-packages
 publishing {
 
     publications {
         register("gprRelease", MavenPublication::class) {
-            groupId = myArtifactGroup
-            artifactId = myArtifactId
-            version = myArtifactVersion
-
+            groupId = project.group.toString()
+            artifactId = rootProject.name
+            version = project.version.toString()
             from(components["java"])
-
-            artifact(sourcesJar)
-
-            pom {
-                packaging = "jar"
-                name.set(myArtifactId)
-                description.set(myGithubDescription)
-                url.set(myGithubHttpUrl)
-                scm {
-                    url.set(myGithubHttpUrl)
-                }
-                issueManagement {
-                    url.set(myGithubIssueTrackerUrl)
-                }
-                licenses {
-                    license {
-                        name.set(myLicense)
-                        url.set(myLicenseUrl)
-                    }
-                }
-                developers {
-                    developer {
-                        id.set(myGithubUsername)
-                        name.set(myDeveloperName)
-                    }
-                }
-            }
-
-        }
-    }
-
-    repositories {
-
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/${myGithubUsername}/${myArtifactId}")
-            credentials {
-                username = System.getenv("GITHUB_PACKAGES_USERID")
-                password = System.getenv("GITHUB_PACKAGES_PUBLISH_TOKEN")
-            }
         }
 
-        maven { url = uri("https://jitpack.io") }
+        repositories {
+            maven { url = uri("https://jitpack.io") }
+        }
 
     }
-
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,16 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.desktop.DesktopMaterialTheme
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+/*
+ * Created by faisalamir on 01/03/22
+ * sample-kotlin-library
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) 2022 FrogoBox Inc.      
+ * All rights reserved
+ *
+ */
 
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    DesktopMaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
-}
-
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
-    }
+fun main() {
+    print(FrogoSettings.NAME_APK)
 }
